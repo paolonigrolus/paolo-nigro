5b3924f2a6716ceff366c79edb85789f05f89e7b
paolonigrolus <127841929+paolonigrolus@users.noreply.github.com>
2023-03-14 11:12:56 +0100

    Add files via upload

---
+++ operatori.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+
+{
+    int eta = 5;
+
+    printf("ciao utente quanti anni hai?\n");
+    scanf("%d", &eta);
+    if (eta < 18)
+    {
+        printf("sei minorenne\n");
+    }
+    else
+    {
+        printf("sei maggiorenne\n");
+    }
+
+
+
+
+}
