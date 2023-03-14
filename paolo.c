983ef3118a403bcd204bea085963837cfd6cbb0a
paolonigrolus <127841929+paolonigrolus@users.noreply.github.com>
2023-03-14 10:50:03 +0100

    Add files via upload

---
+++ paolo.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+int main()
+{
+    int eta;
+    char sesso;
+    printf("ciao utente inserisci la tua eta\n");
+    scanf("%d", &eta);
+    printf("ciao utente inserisci il tuo sesso: F, M o N\n");
+    scanf("%c", &sesso);
+    printf("ciao utente la tua eta e %d e il tuo sesso e %c", eta, sesso);
+
+
+
+
+}
