70f7bb1c8fc3a89520175b777a001ea59fd7cb5a
paolonigrolus <127841929+paolonigrolus@users.noreply.github.com>
2023-03-15 16:22:39 +0100

    Add files via upload

---
+++ array.c
@@ -0,0 +1,35 @@
+#include <stdio.h> 
+
+int main()
+{
+    int i;
+    int a = 2;
+    int z = 0;
+    int divisore = 2;
+    
+    printf("scrivere numero \n");
+    scanf("%d", &i);
+    int array[i];
+
+    
+    while (z<i)
+    {
+        while (a % divisore != 0 && divisore < a)   
+        {    
+            divisore++;
+        }
+        if (divisore == a)
+        {
+            array[z] = a;
+            divisore = 2;
+            z++;
+        }
+        a++;     
+    }    
+    z =0;
+    while (z<i)
+    {
+        printf("%d\n", array[z]);
+        z++;
+    }        
+}
