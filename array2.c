e1c7c1d4bdad1a1087601e21dc96fcb3d2e9e132
paolonigrolus <127841929+paolonigrolus@users.noreply.github.com>
2023-03-15 16:53:54 +0100

    Add files via upload

---
+++ array2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n; 
+    int z = 0;
+    int a;
+    int max = 0;
+    printf("inserire numero di elementi dell'array\n");
+    scanf("%d", &n);
+    int array[n];
+
+    while (z<n)
+    {
+        printf("ïnserire un numero\n");
+        scanf("%d", &a);
+        array[z] = a; 
+        z++; 
+    }
+    z=0;
+    while (z<n)
+    {
+        if ( )
+        
+        
+    }
+}
