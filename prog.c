bdf487bd98238823e1c44161273317e2f325276c
paolonigrolus <127841929+paolonigrolus@users.noreply.github.com>
2023-03-15 12:37:23 +0100

    Add files via upload

---
+++ prog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int x;
+    int y;
+    int z;
+    printf("scrivi i tre numeri\n");
+    scanf("%d" "%d" "%d", &x, &y, &z);
+    
+    if(x-y == y-z) 
+    {
+        printf("è una progressione aritmetica\n");
+    }
+
+    else 
+    {
+        printf("non è una progressione aritmetica\n");
+    }
+
+
+
+
+}
