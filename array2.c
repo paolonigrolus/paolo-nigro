52157e47687c984d95dddf657b06dfefd55d150b
paolonigrolus <127841929+paolonigrolus@users.noreply.github.com>
2023-03-16 11:14:21 +0100

    Add files via upload

--- array2.c
+++ array2.c
@@ -5,11 +5,12 @@ int main()
     int n; 
     int z = 0;
     int a;
-    int max = 0;
+    int max;
+    
     printf("inserire numero di elementi dell'array\n");
     scanf("%d", &n);
-    int array[n];
-
+    int array[n]; 
+    
     while (z<n)
     {
         printf("ïnserire un numero\n");
@@ -18,10 +19,19 @@ int main()
         z++; 
     }
     z=0;
+    max = array[0];
+    a = array[0];
     while (z<n)
     {
-        if ( )
-        
-        
+        if (array[z] > max)
+        {
+            max = array[z];       
+        }
+        else if (array [z] < a)
+        {
+            a = array[z];
+        }
+        z++;
     }
+    printf("il numero piu grande è %d e il piu piccolo è %d\n", max, a);
 }
