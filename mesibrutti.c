9aa353f32243388c58d3aae05489626030768c87
paolonigrolus <127841929+paolonigrolus@users.noreply.github.com>
2023-03-17 17:08:23 +0100

    Add files via upload

---
+++ mesibrutti.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+main()
+{
+    if (mese == 1)
+    {
+        printf("Hai scelto il mese di gennaio!\n");
+    }
+    else if (mese == 2)
+    {
+        printf("Hai scelto il mese di febbraio!\n");
+    }
+    else if (mese == 3)
+    {
+        printf("Hai scelto il mese di marzo!\n");
+    }
+    else if (mese == 4)
+    {
+        printf("Hai scelto il mese di aprile!\n");
+    }
+    else if (mese == 5)
+    {
+        printf("Hai scelto il mese di maggio!\n");
+    }
+    else if (mese == 6)
+    {
+        printf("Hai scelto il mese di giugno!\n");
+    }
+    else if (mese == 7)
+    {
+        printf("Hai scelto il mese di luglio!\n");
+    }
+    else if (mese == 8)
+    {
+        printf("Hai scelto il mese di agosto!\n");
+    }
+    else if (mese == 9)
+    {
+        printf("Hai scelto il mese di settembre!\n");
+    }
+    else if (mese == 10)
+    {
+        printf("Hai scelto il mese di ottobre!\n");
+    }
+    else if (mese == 11)
+    {
+        printf("Hai scelto il mese di novembre!\n");
+    }
+    else if (mese == 12)
+    {
+        printf("Hai scelto il mese di dicembre!\n");
+    }
+    else 
+    {
+        printf("Attenzione! scrivi il numero del mese in cui vuoi cominciare a piantare (es: gennaio = 01)\n");
+    }
+}
+
+
+}
